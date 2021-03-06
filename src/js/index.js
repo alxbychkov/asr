0e7851b23b9327686531ece2864eb445466d23bf
Alexander Bychkov <alx.bychkov@gmail.com>
2021-03-06 16:58:30 +0300

    add last buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,37 @@
 const btnStart = document.querySelector('.asr-btn-start')
 const string = document.querySelector('#string')
+const play = document.querySelectorAll('.track__play')
+const asrBtn = document.querySelector('.asr-btn')
+
+if (asrBtn) {
+    asrBtn.addEventListener('click', e => {
+        const btn = e.target
+        if (btn.classList.contains('asr-btn-start')) {
+            btn.classList.remove('asr-btn-start')
+            btn.classList.add('asr-btn-loading')
+            setTimeout(() => {
+                btn.classList.remove('asr-btn-loading')
+                btn.classList.add('asr-btn-pause')
+                openMain()
+            }, 2500)
+        } else if (btn.classList.contains('asr-btn-pause')) {
+            btn.classList.remove('asr-btn-pause')
+            btn.classList.add('asr-btn-start')
+        }
+    })
+}
 
 if (string) {
     string.addEventListener('change', () => openMain())
 }
 
-if (btnStart) {
-    btnStart.addEventListener('click', () => openMain())
+if (play) {
+    play.forEach(i => {
+        i.addEventListener('click', e => {
+            const btn = e.target
+            !btn.classList.contains('pause') ? btn.classList.add('pause') : btn.classList.remove('pause') 
+        })
+    })
 }
 
 function openMain() {
